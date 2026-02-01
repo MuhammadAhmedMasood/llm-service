e53d3d51a86d991eaf456985058cf6780406b402
Muhammad Ahmed Masood <72328803+MuhammadAhmedMasood@users.noreply.github.com>
2026-02-01 03:56:20 +0000

    Add additional allowed origin for CORS which is the vercel link

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000",
+                  https://llm-service-ui-ahmed.vercel.app/
+    ],
     allow_credentials=True,
     allow_methods=["POST","GET"],
     allow_headers=["*"],
